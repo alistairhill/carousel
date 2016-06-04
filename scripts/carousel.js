407ab733e9e81a403658fe8b0448b86b1e53951b
Alistair <plantonly@gmail.com>
2016-06-04 12:38:13 -0700

    added a bit more oo structure

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,31 +1,64 @@
 window.onload = function() {
-
   var example1 = new alistair.Carousel("example1");
 }
 
 var alistair = alistair || {};
-alistair.Carousel = alistair.Carousel || {};
 
-alistair.Carousel = function(label) {
-  this.label = label;
-  this.slots = document.querySelector("."+ this.label).children;
+alistair.Carousel = function(name, height) {
+  // this.title = title;
+  // this.description = description;
+  this.name = name;
+  this.height = height;
+  this.slide = new alistair.Slide(name);
   this.activate();
 }
-
 alistair.Carousel.prototype = {
   activate: function() {
-    this.setSlideWidth()
+    this.setSlideWidth();
+    this.loadListeners();
   },
-  getSlideWidth: function(marginSize) {
-    var slotsWidth = 0;
-    for (var i = 0; i < this.slots.length; i++) {
-      slotsWidth += (this.slots[i].offsetWidth + 1);
-    }
-    return slotsWidth;
+  loadListeners: function() {
+  },
+  slideMovement: function(direction) {
+  },
+  getCarouselWidth: function() {
+    var caro = document.querySelector(".carousel");
+    return caro.offsetWidth;
   },
   setSlideWidth: function() {
-    var slide = document.querySelector("."+ this.label);
-    var imagesWidth = this.getSlideWidth();
-    slide.style.width = imagesWidth + "px";
+    this.slide.element.style.width = this.slide.width + "px";
+  },
+  setSlidePosition: function(num) {
+    this.slide.element.style.left = num + "px";
+  },
+  resizeSlide: function() {
   }
+}
+
+alistair.Slide = function(name) {
+  this.slots = [];
+  this.width = 1;
+  this.currentSlot = 0;
+  this.leftPosition = 0;
+  this.element = document.querySelector("."+ name);
+  this.makeSlots(name);
+}
+alistair.Slide.prototype = {
+  makeSlots: function(name) {
+    var allSlots = document.querySelector("."+ name).children;
+    for (var i = 0; i < allSlots.length; i++) {
+      this.slots.push(new alistair.Slot(slots[i].offsetWidth));
+    }
+    this.getSlideWidth();
+  },
+  getSlideWidth: function() {
+    for (var i = 0; i < this.slots.length; i++) {
+      this.width += this.slots[i].width;
+    }
+  }
+}
+
+alistair.Slot = function(width) {
+  // this.imageName = imageName;
+  this.width = width;
 }
